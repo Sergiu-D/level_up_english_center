d7dc585c4dd8b2e54682828a227acb711a737ebe
Sergiu-D <61351750+Sergiu-D@users.noreply.github.com>
2024-10-13 12:36:37 +0300

    imp home page sections

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+         <section className="bg-slate-200 !max-w-8xl">First section</section>
+      <section className="bg-slate-500">Second section</section>
+      <section className="bg-slate-700">Third section</section>
+    </>
+  );
+}
