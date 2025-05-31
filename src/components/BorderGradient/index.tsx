b8cae1a6c94034e0a484b1e0623d6d2844f51708
Sergiu-D <61351750+Sergiu-D@users.noreply.github.com>
2025-05-31 10:13:53 +0300

    refactor

--- src/components/BorderGradient/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function BorderGradient({children}) {
-  return (
-    <div className="relative mx-auto rounded-lg bg-gradient-to-tr from-pink-300 to-blue-300 p-0.5 shadow-lg">
-      <div className="bg-gray-50 p-7 rounded-md">{children}</div>
-    </div>
-  );
-}
--- src/messages/variables.js
+++
@@ -1,4 +0,0 @@
-export const variables = {
-  examName: "Wool socks",
-  zeroToForLessons: "Flyers",
-};
