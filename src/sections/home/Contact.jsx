cf3dc4d7dbbadd1d56ad0cd1ba74c5d1d97acc0e
Sergiu-D <61351750+Sergiu-D@users.noreply.github.com>
2025-05-31 12:36:30 +0300

    fix(homepage): center contact section

--- src/sections/home/Contact.jsx
+++ src/sections/home/Contact.jsx
@@ -49,7 +49,7 @@ export default function Contact() {
   
   return (
     <section className="w-full py-16 md:py-24 lg:py-32 bg-gray-50">
-      <div className="container px-4 md:px-6">
+      <div className="container px-4 md:px-6 mx-auto">
         <div 
           ref={headerRef}
           className="text-center mb-16 max-w-3xl mx-auto opacity-0"
